e8086ef520ff08b5d6fb0526cc39cc723cf145f8
omkaruttirnaservice <omkaruttirnaservice@gmail.com>
2025-08-30 11:18:00 +0530

    re

--- web/src/components/TestsList/TestQuestionsView.jsx
+++ web/src/components/TestsList/TestQuestionsView.jsx
@@ -20,21 +20,25 @@ import { EDIT_QUESTION_OF_GENERATED_TEST } from '../Utils/Constants.jsx';
 import CModal from '../UI/CModal.jsx';
 import PDFGenerator from '../Reports/GenerateRports/PDFGen.jsx';
 
+const CIRCLE = 'CIRCLE';
+const TEXT = 'TEXT';
+const NUMBER = 'NUMBER';
+const ROMAN = 'ROMAN';
+
+const optionsInputEnum = [NUMBER, CIRCLE, TEXT, ROMAN];
+
 function TestQuestionsView() {
-    const { questionsList: testQuestionsList, testDetails: previewTestDetails } = useSelector(
-        (state) => state.tests
-    );
-    console.log(previewTestDetails, '=testDetails');
+    const { questionsList, testDetails } = useSelector((state) => state.tests);
 
     const { sendRequest } = useHttp();
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
     useEffect(() => {
-        if (testQuestionsList.length == 0) {
-            dispatch(getQuestionsListThunk(previewTestDetails.test_id, sendRequest, navigate));
+        if (questionsList.length == 0) {
+            dispatch(getQuestionsListThunk(testDetails.test_id, sendRequest, navigate));
         }
-    }, [testQuestionsList]);
+    }, [questionsList]);
 
     useEffect(() => {
         return () => {
@@ -79,7 +83,7 @@ function TestQuestionsView() {
         <>
             <EditQuestionView />
             <CModal id={'view-pdf-modal'} title={'Questions Print List'} className={`min-w-[95vw]`}>
-                <PDFGenerator questions={testQuestionsList} testDetails={previewTestDetails} />
+                <PDFGenerator questions={questionsList} testDetails={testDetails} />
             </CModal>
 
             <CButton
@@ -95,51 +99,45 @@ function TestQuestionsView() {
                     <FaBackspace />
                 </div>
 
-                <H2 className="mb-0">{previewTestDetails.test_name}</H2>
+                <H2 className="mb-0">{testDetails.test_name}</H2>
             </div>
             <div className="container mx-auto grid grid-cols-3 gap-2 mb-6">
-                <PreviewTestDetails title={'Test Duration'} value={previewTestDetails.test_name} />
+                <PreviewTestDetails title={'Test Duration'} value={testDetails.test_name} />
 
                 <PreviewTestDetails
                     title={'Marks per question'}
-                    value={previewTestDetails.marks_per_question}
+                    value={testDetails.marks_per_question}
                 />
 
-                <PreviewTestDetails
-                    title={'Total questions'}
-                    value={previewTestDetails.total_questions}
-                />
+                <PreviewTestDetails title={'Total questions'} value={testDetails.total_questions} />
 
                 <PreviewTestDetails
                     title={'Is negative marking'}
-                    value={previewTestDetails.is_negative_marking == 0 ? 'No' : 'Yes'}
+                    value={testDetails.is_negative_marking == 0 ? 'No' : 'Yes'}
                 />
 
                 <PreviewTestDetails
                     title={'Negative marks'}
-                    value={previewTestDetails.negative_mark == 0 ? 'No' : 'Yes'}
+                    value={testDetails.negative_mark == 0 ? 'No' : 'Yes'}
                 />
-                <PreviewTestDetails
-                    title={'Passing marks'}
-                    value={previewTestDetails.test_passing_mark}
-                />
+                <PreviewTestDetails title={'Passing marks'} value={testDetails.test_passing_mark} />
 
                 <PreviewTestDetails
                     title={'Test created date'}
-                    value={previewTestDetails.test_created_on}
+                    value={testDetails.test_created_on}
                 />
 
-                <PreviewTestDetails title={'Todays date'} value={previewTestDetails.todays_date} />
+                <PreviewTestDetails title={'Todays date'} value={testDetails.todays_date} />
             </div>
 
-            {testQuestionsList.length == 0 && (
+            {questionsList.length == 0 && (
                 <div className="flex justify-center">
                     <FaSpinner className="animate-spin text-2xl" />
                 </div>
             )}
 
             <QuestionSplitView
-                testQuestionsList={testQuestionsList}
+                questionsList={questionsList}
                 renderTopicHeader={renderTopicHeader}
                 handleEditQuestion={handleEditQuestion}
             />
@@ -147,12 +145,12 @@ function TestQuestionsView() {
     );
 }
 
-function QuestionSplitView({ testQuestionsList, renderTopicHeader, handleEditQuestion }) {
+function QuestionSplitView({ questionsList, renderTopicHeader, handleEditQuestion }) {
     return (
         <>
             <div className="container mx-auto columns-2">
-                {testQuestionsList.length >= 1 &&
-                    testQuestionsList.map((el, idx) => {
+                {questionsList.length >= 1 &&
+                    questionsList.map((el, idx) => {
                         const topicHeader = renderTopicHeader(
                             el.main_topic_name,
                             el.sub_topic_section
@@ -173,137 +171,73 @@ function QuestionSplitView({ testQuestionsList, renderTopicHeader, handleEditQue
                                         className={'absolute top-0 right-0 edit-que-btn'}>
                                         Edit
                                     </CButton>
-                                    <div className="py-3 px-4 text-start">
-                                        <div className="py-3">
-                                            <p className="font-bold text-[#555] mb-4 block text-start">
-                                                Q. {idx + 1})
-                                            </p>
-                                            <p
-                                                className="text-start"
-                                                dangerouslySetInnerHTML={{
-                                                    __html: el.q,
-                                                }}></p>
-                                        </div>
 
-                                        <div className="py-3">
-                                            <span className="font-bold text-[#555] mb-4 block text-start">
-                                                Option A
-                                            </span>
-
-                                            <p
-                                                dangerouslySetInnerHTML={{
-                                                    __html: el.q_a,
-                                                }}></p>
-                                        </div>
-
-                                        <hr />
-
-                                        <div className="py-3">
-                                            <span className="font-bold text-[#555] mb-4 block text-start">
-                                                Option B
-                                            </span>
-
-                                            <p
-                                                dangerouslySetInnerHTML={{
-                                                    __html: el.q_b,
-                                                }}></p>
-                                        </div>
-
-                                        <hr />
-
-                                        <div className="py-3">
-                                            <span className="font-bold text-[#555] mb-4 block text-start">
-                                                Option C
-                                            </span>
-                                            <p
-                                                dangerouslySetInnerHTML={{
-                                                    __html: el.q_c,
-                                                }}></p>
-                                        </div>
-
-                                        <hr />
-
-                                        <div className="py-3">
-                                            <span className="font-bold text-[#555] mb-4 block text-start">
-                                                Option D
-                                            </span>
-                                            <p
-                                                dangerouslySetInnerHTML={{
-                                                    __html: el.q_d,
-                                                }}></p>
-                                        </div>
-
-                                        <hr />
-
-                                        {el.q_e && (
-                                            <div className="py-3">
-                                                <span className="font-bold text-[#555] mb-4 block text-start">
-                                                    Option E
-                                                </span>
-                                                <p
-                                                    dangerouslySetInnerHTML={{
-                                                        __html: el.q_e,
-                                                    }}></p>
-                                            </div>
-                                        )}
-
-                                        <hr />
-
-                                        <div className="py-3">
-                                            <span className="font-bold text-[#555] mb-4 me-3">
-                                                Correct Option
-                                            </span>
-                                            <span className="mb-6 bg-blue-200 px-2 py-1 w-fit">
-                                                {el.q_ans.toUpperCase()}
-                                            </span>
-                                        </div>
-
-                                        <hr />
-
-                                        {el.q_sol && (
-                                            <div className="py-3">
-                                                <span className="font-bold text-[#555] my-4 block text-start">
-                                                    Solution
-                                                </span>
-                                                <p
-                                                    className="text-start"
-                                                    dangerouslySetInnerHTML={{
-                                                        __html: el.q_sol,
-                                                    }}></p>
-                                            </div>
-                                        )}
-
-                                        <hr />
-
-                                        <div className=" bg-gray-300 p-3 ">
-                                            <H3>Publication Info</H3>
-
-                                            <table className="w-full">
-                                                <tr>
-                                                    <th className="border px-2 py-1">Pub. Name</th>
-                                                    <th className="border px-2 py-1">Book Name</th>
-                                                    <th className="border px-2 py-1">Pg No</th>
-                                                </tr>
-                                                <tr className="text-center">
-                                                    <td className="border px-2 py-1">
-                                                        {el.pub_name ? el.pub_name : 'NA'}
-                                                    </td>
-                                                    <td className="border px-2 py-1">
-                                                        {el.book_name ? el.book_name : 'NA'}
-                                                    </td>
-                                                    <td className="border px-2 py-1">
-                                                        {el.page_name ? el.page_name : 'NA'}
-                                                    </td>
-                                                </tr>
-                                            </table>
-                                        </div>
-                                    </div>
+                                    <QuestionUi q={el} idx={idx} />
                                 </div>
                             </>
                         );
                     })}
             </div>
         </>
+    );
+}
+
+function QuestionUi({ idx, q }) {
+    return (
+        <div className="border p-4 rounded-md shadow-sm break-inside-avoid mb-4">
+            <div className="font-medium mb-2">
+                Q {idx + 1}.{' '}
+                <div
+                    className="inline-block"
+                    dangerouslySetInnerHTML={{
+                        __html: q?.q || q?.mqs_question || '-',
+                    }}
+                />
+            </div>
+
+            {/* Options */}
+            <div className="pl-4 space-y-1">
+                {(q?.q_a || q?.mqs_opt_one) && (
+                    <QuestionOption option="A" html={q?.q_a || q?.mqs_opt_one || '-'} />
+                )}
+                {(q?.q_b || q?.mqs_opt_two) && (
+                    <QuestionOption option="B" html={q?.q_b || q?.mqs_opt_two || '-'} />
+                )}
+                {(q?.q_c || q?.mqs_opt_three) && (
+                    <QuestionOption option="C" html={q?.q_c || q?.mqs_opt_three || '-'} />
+                )}
+                {(q?.q_d || q?.mqs_opt_four) && (
+                    <QuestionOption option="D" html={q?.q_d || q?.mqs_opt_four || '-'} />
+                )}
+                {(q?.q_e || q?.mqs_opt_five) && (
+                    <QuestionOption option="E" html={q?.q_e || q?.mqs_opt_five || '-'} />
+                )}
+            </div>
+
+            <div className="mt-2 text-sm text-gray-800 border justify-center flex items-center gap-6">
+                <p>
+                    Correct Answer:{' '}
+                    <strong>{q?.q_ans?.toUpperCase() || q?.mqs_ans?.toUpperCase() || '-'}</strong>
+                </p>
+            </div>
+        </div>
+    );
+}
+
+function QuestionOption({ option, html }) {
+    return (
+        <div className="flex gap-2 items-start space-x-2">
+            {/* Option label */}
+            <div className="">{option}.</div>
+
+            {/* Option text */}
+            <div
+                className="inline-block"
+                dangerouslySetInnerHTML={{
+                    __html: html,
+                }}
+            />
+        </div>
     );
 }
 
