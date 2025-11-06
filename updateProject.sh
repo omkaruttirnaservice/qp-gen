ad642b269fa24fb1c4a3ea4b9d5d162ea8525c57
omkaruttirnaservice <omkaruttirnaservice@gmail.com>
2025-11-06 17:20:34 +0530

    feat: add updateProject script for automated deployment and build process

---
+++ updateProject.sh
@@ -0,0 +1,30 @@
+echo "updating..."
+
+ssh dev-server-2 << EOF
+    . ~/.nvm/nvm.sh
+    
+    cd /var/www/ydcc-qp-de/qp
+    git pull
+
+    cd /var/www/ydcc-qp-de/qp/web
+    rm -r dist
+
+    cd /var/www/ydcc-qp-de/qp/web
+    npm run build
+
+    cd /var/www/ydcc-qp-de/de
+    git pull
+
+    cd /var/www/ydcc-qp-de/de/web
+    rm -r dist
+
+    cd /var/www/ydcc-qp-de/qp/web
+    npm run build
+
+    pm2 restart api.ydcc.de.uttirna.in
+    pm2 restart api.ydcc.qp.uttirna.in
+
+EOF
+
+echo "Done updating..."
+
