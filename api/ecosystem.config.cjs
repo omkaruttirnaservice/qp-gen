8cf0a9b8f8233b7929df23d1dec62bcd2c72e057
omkaruttirnaservice <omkaruttirnaservice@gmail.com>
2026-02-27 10:39:23 +0530

    ref

--- api/ecosystem.config.cjs
+++ api/ecosystem.config.cjs
@@ -1,18 +1,16 @@
 module.exports = {
     apps: [
         {
-            name: 'qp_gen_api',
+            name: 'api.qp.uttirna.in',
             script: 'app.js',
             instances: 1,
             autorestart: true,
-            watch: true,
+            watch: false,
             mode: 'fork',
-            max_memory_restart: '2G',
+            max_memory_restart: '500M',
             env: {
-                NODE_ENV: 'development',
             },
             env_production: {
-                NODE_ENV: 'production',
             },
         },
     ],
